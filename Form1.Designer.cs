e196d1db0d78b16a3c40523de782265e202fc58c
deniz.kaparlar <deniz.kaparlar@MER-NB-00779>
2021-08-18 15:21:33 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace SayisalLotoApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sayi1 = new System.Windows.Forms.Label();
+            this.Sayi2 = new System.Windows.Forms.Label();
+            this.Sayi3 = new System.Windows.Forms.Label();
+            this.Sayi4 = new System.Windows.Forms.Label();
+            this.Sayi5 = new System.Windows.Forms.Label();
+            this.Sayi6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Belirle = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // Sayi1
+            // 
+            this.Sayi1.AutoSize = true;
+            this.Sayi1.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi1.Location = new System.Drawing.Point(133, 110);
+            this.Sayi1.Name = "Sayi1";
+            this.Sayi1.Size = new System.Drawing.Size(65, 27);
+            this.Sayi1.TabIndex = 0;
+            this.Sayi1.Text = "Sayi1";
+            // 
+            // Sayi2
+            // 
+            this.Sayi2.AutoSize = true;
+            this.Sayi2.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi2.Location = new System.Drawing.Point(222, 110);
+            this.Sayi2.Name = "Sayi2";
+            this.Sayi2.Size = new System.Drawing.Size(65, 27);
+            this.Sayi2.TabIndex = 1;
+            this.Sayi2.Text = "Sayi2";
+            // 
+            // Sayi3
+            // 
+            this.Sayi3.AutoSize = true;
+            this.Sayi3.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi3.Location = new System.Drawing.Point(311, 110);
+            this.Sayi3.Name = "Sayi3";
+            this.Sayi3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Sayi3.Size = new System.Drawing.Size(65, 27);
+            this.Sayi3.TabIndex = 2;
+            this.Sayi3.Text = "Sayi3";
+            // 
+            // Sayi4
+            // 
+            this.Sayi4.AutoSize = true;
+            this.Sayi4.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi4.Location = new System.Drawing.Point(405, 110);
+            this.Sayi4.Name = "Sayi4";
+            this.Sayi4.Size = new System.Drawing.Size(65, 27);
+            this.Sayi4.TabIndex = 3;
+            this.Sayi4.Text = "Sayi4";
+            // 
+            // Sayi5
+            // 
+            this.Sayi5.AutoSize = true;
+            this.Sayi5.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi5.Location = new System.Drawing.Point(496, 110);
+            this.Sayi5.Name = "Sayi5";
+            this.Sayi5.Size = new System.Drawing.Size(65, 27);
+            this.Sayi5.TabIndex = 4;
+            this.Sayi5.Text = "Sayi5";
+            // 
+            // Sayi6
+            // 
+            this.Sayi6.AutoSize = true;
+            this.Sayi6.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayi6.Location = new System.Drawing.Point(586, 110);
+            this.Sayi6.Name = "Sayi6";
+            this.Sayi6.Size = new System.Drawing.Size(65, 27);
+            this.Sayi6.TabIndex = 5;
+            this.Sayi6.Text = "Sayi6";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(316, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(55, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(227, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(55, 26);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(506, 150);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(55, 26);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(596, 150);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(55, 26);
+            this.textBox6.TabIndex = 11;
+            // 
+            // Belirle
+            // 
+            this.Belirle.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Belirle.Location = new System.Drawing.Point(316, 231);
+            this.Belirle.Name = "Belirle";
+            this.Belirle.Size = new System.Drawing.Size(126, 55);
+            this.Belirle.TabIndex = 12;
+            this.Belirle.Text = "Kontrol Et";
+            this.Belirle.UseVisualStyleBackColor = true;
+            this.Belirle.Click += new System.EventHandler(this.Belirle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 364);
+            this.Controls.Add(this.Belirle);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Sayi6);
+            this.Controls.Add(this.Sayi5);
+            this.Controls.Add(this.Sayi4);
+            this.Controls.Add(this.Sayi3);
+            this.Controls.Add(this.Sayi2);
+            this.Controls.Add(this.Sayi1);
+            this.Name = "Form1";
+            this.Text = "SayisalLoto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Sayi1;
+        private System.Windows.Forms.Label Sayi2;
+        private System.Windows.Forms.Label Sayi3;
+        private System.Windows.Forms.Label Sayi4;
+        private System.Windows.Forms.Label Sayi5;
+        private System.Windows.Forms.Label Sayi6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button Belirle;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayisalLotoApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Belirle_Click(object sender, EventArgs e)
+        {
+            System.Random rnd = new System.Random();
+            int randomsayi1, randomsayi2, randomsayi3, randomsayi4, randomsayi5, randomsayi6;
+
+            randomsayi1 = rnd.Next(1,50);
+            randomsayi2 = rnd.Next(1, 50);
+            randomsayi3 = rnd.Next(1, 50);
+            randomsayi4 = rnd.Next(1, 50);
+            randomsayi5 = rnd.Next(1, 50);
+            randomsayi6 = rnd.Next(1, 50);
+
+            Sayi1.Text = randomsayi1.ToString();
+            Sayi2.Text = randomsayi2.ToString();
+            Sayi3.Text = randomsayi3.ToString();
+            Sayi4.Text = randomsayi4.ToString();
+            Sayi5.Text = randomsayi5.ToString();
+            Sayi6.Text = randomsayi6.ToString();
+
+            if (Sayi1.Text != textBox1.Text)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (Sayi2.Text != textBox2.Text)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (Sayi3.Text != textBox3.Text)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (Sayi4.Text != textBox4.Text)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (Sayi5.Text != textBox5.Text)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (Sayi6.Text != textBox6.Text)
+            {
+                this.BackColor = Color.Red;
+                MessageBox.Show("Loto Tutmadı");
+            }
+            else
+            {
+                this.BackColor = Color.Green;
+            }
+
+
+
+
+        }
+    }
+}
